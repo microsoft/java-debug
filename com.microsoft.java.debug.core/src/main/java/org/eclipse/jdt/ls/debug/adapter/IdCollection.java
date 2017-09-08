14483358701a190801d9c0e3b32111f92411706f
Andy Xu(devdiv) <andxu@microsoft.com>
2017-09-08 13:53:01 +0800

    adjust src from root folder to separated project. (#2)
    * adjust src from root folder to separated project. (#2)
    
    * move src folders
    
    Signed-off-by: andxu <andxu@microsoft.com>

---
+++ com.microsoft.java.debug.core/src/main/java/org/eclipse/jdt/ls/debug/adapter/IdCollection.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+* Copyright (c) 2017 Microsoft Corporation and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*     Microsoft Corporation - initial API and implementation
+*******************************************************************************/
+
+package org.eclipse.jdt.ls.debug.adapter;
+
+import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class IdCollection<T> {
+    private int startId;
+    private AtomicInteger nextId;
+    private HashMap<Integer, T> idMap;
+    private HashMap<T, Integer> reverseMap;
+
+    public IdCollection() {
+        this(1);
+    }
+
+    /**
+     * Constructs a new id generator with the given startId as the start id number.
+     * @param startId
+     *              the start id number
+     */
+    public IdCollection(int startId) {
+        this.startId = startId;
+        this.nextId = new AtomicInteger(startId);
+        this.idMap = new HashMap<>();
+        this.reverseMap = new HashMap<>();
+    }
+
+    /**
+     * Reset the id to the initial start number.
+     */
+    public void reset() {
+        this.nextId.set(this.startId);
+        this.idMap.clear();
+        this.reverseMap.clear();
+    }
+
+    /**
+     * Create a new id if the id doesn't exist for the given value.
+     * Otherwise return the existing id.
+     */
+    public int create(T value) {
+        if (this.reverseMap.containsKey(value)) {
+            return this.reverseMap.get(value);
+        }
+        int id = this.nextId.getAndIncrement();
+        this.idMap.put(id, value);
+        this.reverseMap.put(value, id);
+        return id;
+    }
+
+    /**
+     * Get the original value by the id.
+     */
+    public T get(int id) {
+        return this.idMap.get(id);
+    }
+
+    /**
+     * Remove the id from the id collection.
+     */
+    public T remove(int id) {
+        T target = this.idMap.remove(id);
+        if (target != null) {
+            this.reverseMap.remove(target);
+        }
+        return target;
+    }
+}
---
+++ com.microsoft.java.debug.core/src/main/java/org/eclipse/jdt/ls/debug/adapter/handler/SetBreakpointsRequestHandler.java
@@ -0,0 +1,126 @@
+/*******************************************************************************
+* Copyright (c) 2017 Microsoft Corporation and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*     Microsoft Corporation - initial API and implementation
+*******************************************************************************/
+
+package org.eclipse.jdt.ls.debug.adapter.handler;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.jdt.ls.debug.DebugException;
+import org.eclipse.jdt.ls.debug.IBreakpoint;
+import org.eclipse.jdt.ls.debug.adapter.AdapterUtils;
+import org.eclipse.jdt.ls.debug.adapter.BreakpointManager;
+import org.eclipse.jdt.ls.debug.adapter.ErrorCode;
+import org.eclipse.jdt.ls.debug.adapter.Events;
+import org.eclipse.jdt.ls.debug.adapter.IDebugAdapterContext;
+import org.eclipse.jdt.ls.debug.adapter.IDebugRequestHandler;
+import org.eclipse.jdt.ls.debug.adapter.ISourceLookUpProvider;
+import org.eclipse.jdt.ls.debug.adapter.Messages.Response;
+import org.eclipse.jdt.ls.debug.adapter.Requests.Arguments;
+import org.eclipse.jdt.ls.debug.adapter.Requests.Command;
+import org.eclipse.jdt.ls.debug.adapter.Requests.SetBreakpointArguments;
+import org.eclipse.jdt.ls.debug.adapter.Responses;
+import org.eclipse.jdt.ls.debug.adapter.Types;
+
+public class SetBreakpointsRequestHandler implements IDebugRequestHandler {
+    private BreakpointManager manager = new BreakpointManager();
+
+    @Override
+    public List<Command> getTargetCommands() {
+        return Arrays.asList(Command.SETBREAKPOINTS);
+    }
+
+    @Override
+    public void handle(Command command, Arguments arguments, Response response, IDebugAdapterContext context) {
+        SetBreakpointArguments bpArguments = (SetBreakpointArguments) arguments;
+        String clientPath = bpArguments.source.path;
+        if (AdapterUtils.isWindows()) {
+            // VSCode may send drive letters with inconsistent casing which will mess up the key
+            // in the BreakpointManager. See https://github.com/Microsoft/vscode/issues/6268
+            // Normalize the drive letter casing. Note that drive letters
+            // are not localized so invariant is safe here.
+            String drivePrefix = FilenameUtils.getPrefix(clientPath);
+            if (drivePrefix != null && drivePrefix.length() >= 2
+                    && Character.isLowerCase(drivePrefix.charAt(0)) && drivePrefix.charAt(1) == ':') {
+                drivePrefix = drivePrefix.substring(0, 2); // d:\ is an illegal regex string, convert it to d:
+                clientPath = clientPath.replaceFirst(drivePrefix, drivePrefix.toUpperCase());
+            }
+        }
+        String sourcePath = clientPath;
+        if (bpArguments.source.sourceReference != 0 && context.getSourceUri(bpArguments.source.sourceReference) != null) {
+            sourcePath = context.getSourceUri(bpArguments.source.sourceReference);
+        } else {
+            sourcePath = AdapterUtils.convertPath(clientPath, context.isClientPathsAreUri(), context.isDebuggerPathsAreUri());
+        }
+
+        // When breakpoint source path is null or an invalid file path, send an ErrorResponse back.
+        if (sourcePath == null) {
+            AdapterUtils.setErrorResponse(response, ErrorCode.SET_BREAKPOINT_FAILURE,
+                    String.format("Failed to setBreakpoint. Reason: '%s' is an invalid path.", bpArguments.source.path));
+            return;
+        }
+        try {
+            List<Types.Breakpoint> res = new ArrayList<>();
+            IBreakpoint[] toAdds = this.convertClientBreakpointsToDebugger(sourcePath, bpArguments.breakpoints, context);
+            IBreakpoint[] added = manager.setBreakpoints(sourcePath, toAdds, bpArguments.sourceModified);
+            for (int i = 0; i < bpArguments.breakpoints.length; i++) {
+                // For newly added breakpoint, should install it to debuggee first.
+                if (toAdds[i] == added[i] && added[i].className() != null) {
+                    added[i].install().thenAccept(bp -> {
+                        Events.BreakpointEvent bpEvent = new Events.BreakpointEvent("new", this.convertDebuggerBreakpointToClient(bp, context));
+                        context.sendEventAsync(bpEvent);
+                    });
+                } else if (toAdds[i].hitCount() != added[i].hitCount() && added[i].className() != null) {
+                    // Update hitCount condition.
+                    added[i].setHitCount(toAdds[i].hitCount());
+                }
+                res.add(this.convertDebuggerBreakpointToClient(added[i], context));
+            }
+            response.body = new Responses.SetBreakpointsResponseBody(res);
+        } catch (DebugException e) {
+            AdapterUtils.setErrorResponse(response, ErrorCode.SET_BREAKPOINT_FAILURE, String.format("Failed to setBreakpoint. Reason: '%s'", e.toString()));
+        }
+    }
+
+    private Types.Breakpoint convertDebuggerBreakpointToClient(IBreakpoint breakpoint, IDebugAdapterContext context) {
+        int id = (int) breakpoint.getProperty("id");
+        boolean verified = breakpoint.getProperty("verified") != null && (boolean) breakpoint.getProperty("verified");
+        int lineNumber = AdapterUtils.convertLineNumber(breakpoint.lineNumber(), context.isDebuggerLinesStartAt1(), context.isClientLinesStartAt1());
+        return new Types.Breakpoint(id, verified, lineNumber, "");
+    }
+
+    private IBreakpoint[] convertClientBreakpointsToDebugger(String sourceFile, Types.SourceBreakpoint[] sourceBreakpoints, IDebugAdapterContext context)
+            throws DebugException {
+        int[] lines = Arrays.asList(sourceBreakpoints).stream().map(sourceBreakpoint -> {
+            return AdapterUtils.convertLineNumber(sourceBreakpoint.line, context.isClientLinesStartAt1(), context.isDebuggerLinesStartAt1());
+        }).mapToInt(line -> line).toArray();
+        ISourceLookUpProvider sourceProvider = context.getProvider(ISourceLookUpProvider.class);
+        String[] fqns = sourceProvider.getFullyQualifiedName(sourceFile, lines, null);
+        IBreakpoint[] breakpoints = new IBreakpoint[lines.length];
+        for (int i = 0; i < lines.length; i++) {
+            int hitCount = 0;
+            try {
+                hitCount = Integer.parseInt(sourceBreakpoints[i].hitCondition);
+            } catch (NumberFormatException e) {
+                hitCount = 0; // If hitCount is an illegal number, ignore hitCount condition.
+            }
+            breakpoints[i] = context.getDebugSession().createBreakpoint(fqns[i], lines[i], hitCount);
+            if (sourceProvider.supportsRealtimeBreakpointVerification() && StringUtils.isNotBlank(fqns[i])) {
+                breakpoints[i].putProperty("verified", true);
+            }
+        }
+        return breakpoints;
+    }
+
+}
