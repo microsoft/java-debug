d2c8f8da19035303e9856dbff8e0026b42390f63
Jinbo Wang <jinbwan@microsoft.com>
2017-09-11 12:24:37 +0800

    fix installjstls script

--- installjdtls.bat
+++ installjdtls.bat
@@ -1,11 +1,12 @@
 cd %~dp0
-bitsadmin.exe /transfer "downloadjdtls"/download /priority FOREGROUND https://vscjavaci.cloudapp.net/job/yaohai_jdtls_gzsnapshot/4/Azure/processDownloadRequest/yaohai/jdt-language-server-latest.tar.gz "%cd%/jdt-language-server-latest.tar.gz"
+bitsadmin.exe /transfer "downloadjdtls"/download /priority FOREGROUND https://vscjavaci.cloudapp.net/job/yaohai_jdtls_gzsnapshot/lastSuccessfulBuild/Azure/processDownloadRequest/yaohai/jdt-language-server-latest.tar.gz "%cd%/jdt-language-server-latest.tar.gz"
 if exist jdtls (
     rm -rf jdtls/
 )
 mkdir jdtls
 tar -xzf jdt-language-server-latest.tar.gz -C jdtls/
-for /R ./jdtls/plugins %%F in (org.eclipse.jdt.ls.core_*.jar) do (
+cd jdtls
+for /R ./plugins %%F in (org.eclipse.jdt.ls.core_*.jar) do (
     echo %%F
     mvn install:install-file -Dfile="%%F" -DgroupId=org.eclipse.jdt.ls -DartifactId=org.eclipse.jdt.ls.core -Dversion=0.4.0 -Dpackaging=jar
 )
--- installjdtls.sh
+++ installjdtls.sh
@@ -1,8 +1,10 @@
 dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
-wget https://vscjavaci.cloudapp.net/job/yaohai_jdtls_gzsnapshot/4/Azure/processDownloadRequest/yaohai/jdt-language-server-latest.tar.gz
 rm -rf jdtls/
+rm -rf jdt-language-server-latest.tar.gz
+wget https://vscjavaci.cloudapp.net/job/yaohai_jdtls_gzsnapshot/lastSuccessfulBuild/Azure/processDownloadRequest/yaohai/jdt-language-server-latest.tar.gz
 mkdir jdtls
 tar -xzf jdt-language-server-latest.tar.gz -C jdtls/
-jdtlscorejar=`find jdtls/ -name 'org.eclipse.jdt.ls.core_*.jar'`
+cd jdtls
+jdtlscorejar=`find . -name 'org.eclipse.jdt.ls.core_*.jar'`
 mvn install:install-file -Dfile=$jdtlscorejar -DgroupId=org.eclipse.jdt.ls -DartifactId=org.eclipse.jdt.ls.core -Dversion=0.4.0 -Dpackaging=jar
