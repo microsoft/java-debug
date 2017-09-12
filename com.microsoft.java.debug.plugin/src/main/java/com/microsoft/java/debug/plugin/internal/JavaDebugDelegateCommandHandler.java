e1fa204e7de615fc8f65433b0f1d08311bbdd37b
Jinbo Wang <jinbwan@microsoft.com>
2017-09-12 17:29:22 +0800

    Update delegate commander to use new interface
    Signed-off-by: Jinbo Wang <jinbwan@microsoft.com>

--- com.microsoft.java.debug.plugin/src/main/java/com/microsoft/java/debug/plugin/internal/JavaDebugDelegateCommandHandler.java
+++ com.microsoft.java.debug.plugin/src/main/java/com/microsoft/java/debug/plugin/internal/JavaDebugDelegateCommandHandler.java
@@ -1,38 +1,42 @@
+/*******************************************************************************
+* Copyright (c) 2017 Microsoft Corporation and others.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*     Microsoft Corporation - initial API and implementation
+*******************************************************************************/
+
 package com.microsoft.java.debug.plugin.internal;
 
 import java.util.List;
 
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jdt.ls.core.internal.IDelegateCommandHandler;
 
+public class JavaDebugDelegateCommandHandler implements IDelegateCommandHandler {
 
-public class JavaDebugDelegateCommandHandler implements  IDelegateCommandHandler {
+    public static String DEBUG_STARTSESSION = "vscode.java.startDebugSession";
 
-	public static String DEBUG_STARTSESSION = "vscode.java.startDebugSession";
+    public static String RESOLVE_CLASSPATH = "vscode.java.resolveClasspath";
 
-	public static String RESOLVE_CLASSPATH = "vscode.java.resolveClasspath";
+    public static String BUILD_WORKSPACE = "vscode.java.buildWorkspace";
 
-	public static String BUILD_WORKSPACE = "vscode.java.buildWorkspace";
+    @Override
+    public Object executeCommand(String commandId, List<Object> arguments, IProgressMonitor progress) throws Exception {
+        if (DEBUG_STARTSESSION.equals(commandId)) {
+            IDebugServer debugServer = JavaDebugServer.getInstance();
+            debugServer.start();
+            return debugServer.getPort();
+        } else if (RESOLVE_CLASSPATH.equals(commandId)) {
+            ResolveClasspathsHandler handler = new ResolveClasspathsHandler();
+            return handler.resolveClasspaths(arguments);
+        } else if (BUILD_WORKSPACE.equals(commandId)) {
+            // TODO
+        }
+        throw new UnsupportedOperationException(String.format("Java debug plugin doesn't support the command '%s'.", commandId));
+    }
 
-	@Override
-	public Object executeCommand(String commandId, List<Object> arguments) {
-		if (DEBUG_STARTSESSION.equals(commandId)) {
-		    IDebugServer debugServer = JavaDebugServer.getInstance();
-		    debugServer.start();
-		    return debugServer.getPort();
-		} else if (RESOLVE_CLASSPATH.equals(commandId)) {
-			ResolveClasspathsHandler handler = new ResolveClasspathsHandler();
-			return handler.resolveClasspaths(arguments);
-		} else if (BUILD_WORKSPACE.equals(commandId)) {
-			
-		}
-		// TODO language server jsonRpc doesn't support null value as response.
-		// To workaround it, return an empty string as response.
-		// Will revisit lsp4j and vscode-jsonrpc projects to see if it's a json-rpc bug.
-		return "";
-	}
-	
-	public static JavaDebugDelegateCommandHandler  getInstance() {
-		return new JavaDebugDelegateCommandHandler();
-	}
 }
-	
--- com.microsoft.java.debug.plugin/src/main/java/com/microsoft/java/debug/plugin/internal/JavaDebuggerServerPlugin.java
+++ com.microsoft.java.debug.plugin/src/main/java/com/microsoft/java/debug/plugin/internal/JavaDebuggerServerPlugin.java
@@ -11,22 +11,27 @@
 
 package com.microsoft.java.debug.plugin.internal;
 
+import java.util.logging.Logger;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+import com.microsoft.java.debug.core.Configuration;
+
 public class JavaDebuggerServerPlugin implements BundleActivator {
+    private static final Logger logger = Logger.getLogger(Configuration.LOGGER_NAME);
 
     public static final String PLUGIN_ID = "com.microsoft.java.debug";
 
 
     @Override
     public void start(BundleContext context) throws Exception {
-    	System.out.println("Starting " + PLUGIN_ID);
+    	logger.info("Starting " + PLUGIN_ID);
     }
 
     @Override
     public void stop(BundleContext context) throws Exception {
-
+        logger.info("Stopping " + PLUGIN_ID);
     }
 
 }
