ee0fc25cf9a2043bd83b42aae017b9c435ac73ed
Jinbo Wang <jinbwan@microsoft.com>
2017-09-13 13:59:18 +0800

    fix install jdtls script

--- installjdtls.bat
+++ installjdtls.bat
@@ -5,8 +5,10 @@ if exist jdtls (
 )
 mkdir jdtls
 tar -xzf jdt-language-server-latest.tar.gz -C jdtls/
+set workspaceRoot=%~dp0
 cd jdtls
 for /R ./plugins %%F in (org.eclipse.jdt.ls.core_*.jar) do (
     echo %%F
-    mvn install:install-file -Dfile="%%F" -DgroupId=org.eclipse.jdt.ls -DartifactId=org.eclipse.jdt.ls.core -Dversion=0.4.0 -Dpackaging=jar
+    %workspaceRoot%/mvnw.cmd install:install-file -Dfile="%%F" -DgroupId=org.eclipse.jdt.ls -DartifactId=org.eclipse.jdt.ls.core -Dversion=0.4.0 -Dpackaging=jar
 )
+cd %workspaceRoot%
