e324df783651c012a22336afda7b90843b229aa2
Jinbo Wang <jinbwan@microsoft.com>
2017-09-14 10:42:38 +0800

    add .travis.yml and maven wrapper (#19)
    * add wrapper for maven-3.5.0
    
    Signed-off-by: Jinbo Wang <jinbwan@microsoft.com>
    
    * add .travis.yml
    
    Signed-off-by: Jinbo Wang <jinbwan@microsoft.com>
    
    * fix install jdtls script
    
    * update exec permission for shell script
    
    * add maven-wrapper.jar
    
    * move installjdtls script to before_install step

--- installjdtls.sh
+++ installjdtls.sh
@@ -1,4 +1,4 @@
-dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+workspaceRoot=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 rm -rf jdtls/
 rm -rf jdt-language-server-latest.tar.gz
@@ -7,4 +7,5 @@ mkdir jdtls
 tar -xzf jdt-language-server-latest.tar.gz -C jdtls/
 cd jdtls
 jdtlscorejar=`find . -name 'org.eclipse.jdt.ls.core_*.jar'`
-mvn install:install-file -Dfile=$jdtlscorejar -DgroupId=org.eclipse.jdt.ls -DartifactId=org.eclipse.jdt.ls.core -Dversion=0.4.0 -Dpackaging=jar
+$workspaceRoot/mvnw install:install-file -Dfile=$jdtlscorejar -DgroupId=org.eclipse.jdt.ls -DartifactId=org.eclipse.jdt.ls.core -Dversion=0.4.0 -Dpackaging=jar
+cd $workspaceRoot
