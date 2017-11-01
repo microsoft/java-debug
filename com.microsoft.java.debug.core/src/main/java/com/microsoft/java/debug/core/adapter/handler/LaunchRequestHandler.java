20f3fefef28f7ba263a5ca437068eaf6fae8e27e
Andy Xu(devdiv) <andxu@microsoft.com>
2017-11-01 11:21:03 +0800

    Update LaunchRequestHandler.java

--- com.microsoft.java.debug.core/src/main/java/com/microsoft/java/debug/core/adapter/handler/LaunchRequestHandler.java
+++ com.microsoft.java.debug.core/src/main/java/com/microsoft/java/debug/core/adapter/handler/LaunchRequestHandler.java
@@ -21,7 +21,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.logging.Logger;
 
 import org.apache.commons.lang3.StringUtils;
 
