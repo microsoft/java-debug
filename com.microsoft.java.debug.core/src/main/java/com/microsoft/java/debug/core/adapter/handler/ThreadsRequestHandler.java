7878d8169364c4743c52e2a61deda41770d7b845
Jinbo Wang <jinbwan@microsoft.com>
2017-11-22 10:54:21 +0800

    Remove unnecessary blank space
    Signed-off-by: Jinbo Wang <jinbwan@microsoft.com>

--- com.microsoft.java.debug.core/src/main/java/com/microsoft/java/debug/core/adapter/handler/ThreadsRequestHandler.java
+++ com.microsoft.java.debug.core/src/main/java/com/microsoft/java/debug/core/adapter/handler/ThreadsRequestHandler.java
@@ -16,7 +16,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import java.util.concurrent.CompletableFuture;
 
 import com.microsoft.java.debug.core.DebugEvent;
