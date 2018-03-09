6fbe232a136f1fb1905ef3c0bbf1eb782b8f99d3
andxu <andxu@microsoft.com>
2018-03-09 15:06:59 +0800

    remove an empty line.

--- com.microsoft.java.debug.core/src/main/java/com/microsoft/java/debug/core/adapter/handler/SetBreakpointsRequestHandler.java
+++ com.microsoft.java.debug.core/src/main/java/com/microsoft/java/debug/core/adapter/handler/SetBreakpointsRequestHandler.java
@@ -76,7 +76,6 @@ public class SetBreakpointsRequestHandler implements IDebugRequestHandler {
                     logger.severe(e.toString());
                 }
             });
-
     }
 
     @Override
