67fb7fb6fa91fc951c76530f2cae00581b204e53
Jinbo Wang <jinbwan@microsoft.com>
2018-08-15 09:37:27 +0800

    Validate mainClass and projectName configs
    Signed-off-by: Jinbo Wang <jinbwan@microsoft.com>

--- com.microsoft.java.debug.plugin/src/main/java/com/microsoft/java/debug/plugin/internal/ResolveMainClassHandler.java
+++ com.microsoft.java.debug.plugin/src/main/java/com/microsoft/java/debug/plugin/internal/ResolveMainClassHandler.java
@@ -12,12 +12,16 @@
 package com.microsoft.java.debug.plugin.internal;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
@@ -40,19 +44,35 @@ import com.microsoft.java.debug.core.Configuration;
 
 public class ResolveMainClassHandler {
     private static final Logger logger = Logger.getLogger(Configuration.LOGGER_NAME);
+    private static final String CLASSNAME_REGX = "([$\\w]+\\.)*[$\\w]+";
 
     /**
      * resolve main class and project name.
      * @return an array of main class and project name
-     * @throws CoreException when there are errors when resolving main class.
      */
-    public Object resolveMainClass(List<Object> arguments) throws CoreException {
+    public Object resolveMainClass(List<Object> arguments) {
         return resolveMainClassCore(arguments);
     }
 
-    private List<ResolutionItem> resolveMainClassCore(List<Object> arguments) throws CoreException {
+    /**
+     * Validate whether the mainClass and projectName is correctly configured or not. If not, report the validation error and provide the quick fix proposal.
+     *
+     * @param arguments the mainClass and projectName configs.
+     * @return the validation response.
+     * @throws Exception when there are any errors during validating the mainClass and projectName.
+     */
+    public Object validateLaunchConfig(List<Object> arguments) throws Exception {
+        try {
+            return validateLaunchConfigCore(arguments);
+        } catch (CoreException ex) {
+            logger.log(Level.SEVERE, "Failed to validate launch config: " + ex.getMessage(), ex);
+            throw new Exception("Failed to validate launch config: " + ex.getMessage(), ex);
+        }
+    }
+
+    private List<ResolutionItem> resolveMainClassCore(List<Object> arguments) {
         IPath rootPath = null;
-        if (arguments != null && arguments.size() > 0) {
+        if (arguments != null && arguments.size() > 0 && arguments.get(0) != null) {
             rootPath = ResourceUtils.filePathFromURI((String) arguments.get(0));
         }
         final ArrayList<IPath> targetProjectPath = new ArrayList<>();
@@ -62,7 +82,7 @@ public class ResolveMainClassHandler {
         IJavaSearchScope searchScope = SearchEngine.createWorkspaceScope();
         SearchPattern pattern = SearchPattern.createPattern("main(String[]) void", IJavaSearchConstants.METHOD,
                 IJavaSearchConstants.DECLARATIONS, SearchPattern.R_CASE_SENSITIVE | SearchPattern.R_EXACT_MATCH);
-        ArrayList<ResolutionItem> res = new ArrayList<>();
+        final List<ResolutionItem> res = new ArrayList<>();
         SearchRequestor requestor = new SearchRequestor() {
             @Override
             public void acceptSearchMatch(SearchMatch match) {
@@ -114,10 +134,92 @@ public class ResolveMainClassHandler {
         } catch (Exception e) {
             logger.log(Level.SEVERE, String.format("Searching the main class failure: %s", e.toString()), e);
         }
-        return res.stream().distinct().collect(Collectors.toList());
+
+        List<ResolutionItem> resolutions = res.stream().distinct().collect(Collectors.toList());
+        Collections.sort(resolutions);
+        return resolutions;
     }
 
-    private class ResolutionItem {
+    private ValidationResponse validateLaunchConfigCore(List<Object> arguments) throws CoreException {
+        ValidationResponse response = new ValidationResponse();
+
+        String mainClass = null;
+        String projectName = null;
+        if (arguments != null) {
+            if (arguments.size() > 1) {
+                mainClass = (String) arguments.get(1);
+            }
+            if (arguments.size() > 2) {
+                projectName = (String) arguments.get(2);
+            }
+        }
+
+        response.mainClass = validateMainClass(mainClass, projectName);
+        response.projectName = validateProjectName(mainClass, projectName);
+
+        if (!response.mainClass.passed || !response.projectName.passed) {
+            response.proposals = computeProposals(arguments, mainClass, projectName);
+        }
+
+        return response;
+    }
+
+    private ValidationResult validateMainClass(final String mainClass, final String projectName) throws CoreException {
+        if (StringUtils.isEmpty(mainClass)) {
+            return new ValidationResult(true);
+        } else if (!mainClass.matches(CLASSNAME_REGX)) {
+            return new ValidationResult(false, String.format("ConfigError: '%s' is not a valid class name.", mainClass));
+        }
+
+        List<IJavaProject> javaProjects = ResolveClasspathsHandler.getJavaProjectFromType(mainClass);
+        if (javaProjects.size() == 0) {
+            return new ValidationResult(false, String.format("ConfigError: Main class '%s' doesn't exist in the workspace.", mainClass));
+        } else if (javaProjects.size() > 1 && !containsProject(javaProjects, projectName)) {
+            return new ValidationResult(false, String.format("ConfigError: Main class '%s' isn't unique in the workspace.", mainClass));
+        }
+
+        return new ValidationResult(true);
+    }
+
+    private boolean containsProject(List<IJavaProject> javaProjects, String projectName) {
+        for (IJavaProject javaProject : javaProjects) {
+            if (javaProject.getProject().getName().equals(projectName)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private ValidationResult validateProjectName(final String mainClass, final String projectName) {
+        if (StringUtils.isEmpty(projectName)) {
+            return new ValidationResult(true);
+        }
+
+        if (JdtUtils.getJavaProject(projectName) == null) {
+            return new ValidationResult(false, String.format("ConfigError: The project '%s' is not a valid java project.", projectName));
+        }
+
+        return new ValidationResult(true);
+    }
+
+    private List<ResolutionItem> computeProposals(List<Object> arguments, final String mainClass, final String projectName) {
+        List<ResolutionItem> proposals = resolveMainClassCore(arguments);
+
+        Collections.sort(proposals, new ProposalItemComparator((ResolutionItem item) -> {
+            if (item.mainClass.equals(mainClass)) {
+                return 1;
+            } else if (item.projectName != null && item.projectName.equals(projectName)) {
+                return 2;
+            }
+
+            return 999;
+        }));
+
+        return proposals;
+    }
+
+    private class ResolutionItem implements Comparable<ResolutionItem> {
         private String mainClass;
         private String projectName;
         private String filePath;
@@ -145,6 +247,54 @@ public class ResolveMainClassHandler {
         @Override
         public int hashCode() {
             return Objects.hash(mainClass, projectName, filePath);
+        }
+
+        @Override
+        public int compareTo(ResolutionItem o) {
+            if (isDefaultProject(this.projectName) && !isDefaultProject(o.projectName)) {
+                return 1;
+            } else if (!isDefaultProject(this.projectName) && isDefaultProject(o.projectName)) {
+                return -1;
+            }
+
+            return (this.projectName + "|" + this.mainClass).compareTo(o.projectName + "|" + o.mainClass);
+        }
+
+        private boolean isDefaultProject(String projectName) {
+            return StringUtils.isEmpty(projectName);
+        }
+    }
+
+    class ProposalItemComparator implements Comparator<ResolutionItem> {
+        Function<ResolutionItem, Integer> getRank;
+
+        ProposalItemComparator(Function<ResolutionItem, Integer> getRank) {
+            this.getRank = getRank;
+        }
+
+        @Override
+        public int compare(ResolutionItem o1, ResolutionItem o2) {
+            return getRank.apply(o1) - getRank.apply(o2);
+        }
+    }
+
+    class ValidationResponse {
+        ValidationResult mainClass;
+        ValidationResult projectName;
+        List<ResolutionItem> proposals;
+    }
+
+    class ValidationResult {
+        boolean passed;
+        String message;
+
+        ValidationResult(boolean passed) {
+            this.passed = passed;
+        }
+
+        ValidationResult(boolean passed, String message) {
+            this.passed = passed;
+            this.message = message;
         }
     }
 }
