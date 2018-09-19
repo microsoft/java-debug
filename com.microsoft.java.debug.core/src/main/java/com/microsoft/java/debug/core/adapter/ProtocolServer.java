6cd1c7db21caa9d15910106fc56220c64d065d18
Jinbo Wang <jinbwan@microsoft.com>
2018-09-19 14:44:20 +0800

    Fix debug buttons disabled issue: send StoppeEvent after StepResponse/ContinuedResponse
    Signed-off-by: Jinbo Wang <jinbwan@microsoft.com>

--- com.microsoft.java.debug.core/src/main/java/com/microsoft/java/debug/core/adapter/ProtocolServer.java
+++ com.microsoft.java.debug.core/src/main/java/com/microsoft/java/debug/core/adapter/ProtocolServer.java
@@ -15,6 +15,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -22,6 +23,8 @@ import com.microsoft.java.debug.core.Configuration;
 import com.microsoft.java.debug.core.DebugException;
 import com.microsoft.java.debug.core.UsageDataSession;
 import com.microsoft.java.debug.core.protocol.AbstractProtocolServer;
+import com.microsoft.java.debug.core.protocol.Events.DebugEvent;
+import com.microsoft.java.debug.core.protocol.Events.StoppedEvent;
 import com.microsoft.java.debug.core.protocol.Messages;
 import com.sun.jdi.VMDisconnectedException;
 
@@ -30,6 +33,8 @@ public class ProtocolServer extends AbstractProtocolServer {
 
     private IDebugAdapter debugAdapter;
     private UsageDataSession usageDataSession = new UsageDataSession();
+    private boolean isDispatchingRequest = false;
+    private ConcurrentLinkedQueue<DebugEvent> eventQueue = new ConcurrentLinkedQueue<>();
 
     /**
      * Constructs a protocol server instance based on the given input stream and output stream.
@@ -75,43 +80,87 @@ public class ProtocolServer extends AbstractProtocolServer {
     }
 
     @Override
+    public void sendEvent(DebugEvent event) {
+        // See the two bugs https://github.com/Microsoft/java-debug/issues/134 and https://github.com/Microsoft/vscode/issues/58327,
+        // it requires the java-debug to send the StoppedEvent after ContinueResponse/StepResponse is received by DA.
+        if (event instanceof StoppedEvent) {
+            sendEventLater(event);
+        } else {
+            super.sendEvent(event);
+        }
+
+    }
+
+    /**
+     * If the the dispatcher is idle, then send the event to the DA immediately.
+     * Else add the new event to an eventQueue first and send them when dispatcher becomes idle again.
+     * @param eventType
+     *              event type
+     * @param body
+     *              event content
+     */
+    private void sendEventLater(DebugEvent event) {
+        synchronized (this) {
+            if (this.isDispatchingRequest) {
+                this.eventQueue.offer(event);
+            } else {
+                super.sendEvent(event);
+            }
+        }
+    }
+
+    @Override
     protected void dispatchRequest(Messages.Request request) {
         usageDataSession.recordRequest(request);
-        debugAdapter.dispatchRequest(request).thenCompose((response) -> {
-            CompletableFuture<Void> future = new CompletableFuture<>();
-            if (response != null) {
-                sendResponse(response);
-                future.complete(null);
-            } else {
-                future.completeExceptionally(new DebugException("The request dispatcher should not return null response.",
-                    ErrorCode.UNKNOWN_FAILURE.getId()));
-            }
-            return future;
-        }).exceptionally((ex) -> {
-            Messages.Response response = new Messages.Response(request.seq, request.command);
-            if (ex instanceof CompletionException && ex.getCause() != null) {
-                ex = ex.getCause();
+        try {
+            synchronized (this) {
+                this.isDispatchingRequest = true;
             }
 
-            if (ex instanceof VMDisconnectedException) {
-                // mark it success to avoid reporting error on VSCode.
-                response.success = true;
-                sendResponse(response);
-            } else {
-                String exceptionMessage = ex.getMessage() != null ? ex.getMessage() : ex.toString();
-                ErrorCode errorCode = ex instanceof DebugException ? ErrorCode.parse(((DebugException) ex).getErrorCode()) : ErrorCode.UNKNOWN_FAILURE;
-                boolean isUserError = ex instanceof DebugException && ((DebugException) ex).isUserError();
-                if (isUserError) {
-                    usageDataSession.recordUserError(errorCode);
+            debugAdapter.dispatchRequest(request).thenCompose((response) -> {
+                CompletableFuture<Void> future = new CompletableFuture<>();
+                if (response != null) {
+                    sendResponse(response);
+                    future.complete(null);
                 } else {
-                    logger.log(Level.SEVERE, String.format("[error response][%s]: %s", request.command, exceptionMessage), ex);
+                    future.completeExceptionally(new DebugException("The request dispatcher should not return null response.",
+                            ErrorCode.UNKNOWN_FAILURE.getId()));
+                }
+                return future;
+            }).exceptionally((ex) -> {
+                Messages.Response response = new Messages.Response(request.seq, request.command);
+                if (ex instanceof CompletionException && ex.getCause() != null) {
+                    ex = ex.getCause();
                 }
 
-                sendResponse(AdapterUtils.setErrorResponse(response,
-                    errorCode,
-                    exceptionMessage));
+                if (ex instanceof VMDisconnectedException) {
+                    // mark it success to avoid reporting error on VSCode.
+                    response.success = true;
+                    sendResponse(response);
+                } else {
+                    String exceptionMessage = ex.getMessage() != null ? ex.getMessage() : ex.toString();
+                    ErrorCode errorCode = ex instanceof DebugException ? ErrorCode.parse(((DebugException) ex).getErrorCode()) : ErrorCode.UNKNOWN_FAILURE;
+                    boolean isUserError = ex instanceof DebugException && ((DebugException) ex).isUserError();
+                    if (isUserError) {
+                        usageDataSession.recordUserError(errorCode);
+                    } else {
+                        logger.log(Level.SEVERE, String.format("[error response][%s]: %s", request.command, exceptionMessage), ex);
+                    }
+
+                    sendResponse(AdapterUtils.setErrorResponse(response,
+                            errorCode,
+                            exceptionMessage));
+                }
+                return null;
+            }).join();
+        } finally {
+            synchronized (this) {
+                this.isDispatchingRequest = false;
             }
-            return null;
-        }).join();
+
+            while (this.eventQueue.peek() != null) {
+                super.sendEvent(this.eventQueue.poll());
+            }
+        }
     }
 }
