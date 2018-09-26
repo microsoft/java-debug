1320d76408ec092e9e7075347d5ec9252753ea0c
andxu <andxu@microsoft.com>
2018-09-26 16:37:09 +0800

    fix the bug: Cannot run from src/test anymore bug #375

--- com.microsoft.java.debug.plugin/src/main/java/com/microsoft/java/debug/plugin/internal/ResolveClasspathsHandler.java
+++ com.microsoft.java.debug.plugin/src/main/java/com/microsoft/java/debug/plugin/internal/ResolveClasspathsHandler.java
@@ -26,6 +26,8 @@ import org.eclipse.jdt.core.IClasspathAttribute;
 import org.eclipse.jdt.core.IClasspathEntry;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.search.IJavaSearchConstants;
 import org.eclipse.jdt.core.search.IJavaSearchScope;
 import org.eclipse.jdt.core.search.SearchEngine;
@@ -40,13 +42,18 @@ import com.microsoft.java.debug.core.Configuration;
 
 public class ResolveClasspathsHandler {
     private static final Logger logger = Logger.getLogger(Configuration.LOGGER_NAME);
-    private static final String SCOPE_ATTRIBUTE = "maven.scope";
+    private static final String TEST_SCOPE = "test";
+    private static final String MAVEN_SCOPE_ATTRIBUTE = "maven.scope";
+    private static final String GRADLE_SCOPE_ATTRIBUTE = "gradle_scope";
 
     /**
      * Resolves class path for a java project.
-     * @param arguments a list contains the main class name and  project name
+     *
+     * @param arguments
+     *            a list contains the main class name and project name
      * @return the class paths entries
-     * @throws Exception when there are any errors during resolving class path
+     * @throws Exception
+     *             when there are any errors during resolving class path
      */
     public String[][] resolveClasspaths(List<Object> arguments) throws Exception {
         try {
@@ -92,11 +99,8 @@ public class ResolveClasspathsHandler {
         }
         final String moduleName = splitItems.length == 2 ? splitItems[0] : null;
 
-        SearchPattern pattern = SearchPattern.createPattern(
-                fullyQualifiedTypeName,
-                IJavaSearchConstants.TYPE,
-                IJavaSearchConstants.DECLARATIONS,
-                SearchPattern.R_EXACT_MATCH);
+        SearchPattern pattern = SearchPattern.createPattern(fullyQualifiedTypeName, IJavaSearchConstants.TYPE,
+                IJavaSearchConstants.DECLARATIONS, SearchPattern.R_EXACT_MATCH);
         IJavaSearchScope scope = SearchEngine.createWorkspaceScope();
         ArrayList<IJavaProject> projects = new ArrayList<>();
         SearchRequestor requestor = new SearchRequestor() {
@@ -113,8 +117,8 @@ public class ResolveClasspathsHandler {
         };
         SearchEngine searchEngine = new SearchEngine();
         searchEngine.search(pattern, new SearchParticipant[] {
-            SearchEngine.getDefaultSearchParticipant() }, scope,
-            requestor, null /* progress monitor */);
+                SearchEngine.getDefaultSearchParticipant()
+            }, scope, requestor, null /* progress monitor */);
 
         return projects.stream().distinct().collect(Collectors.toList());
     }
@@ -150,7 +154,8 @@ public class ResolveClasspathsHandler {
             }
             project = projects.get(0);
         }
-        return computeClassPath(project);
+
+        return computeClassPath(project, isMainClassInTestFolder(project, mainClass));
     }
 
     /**
@@ -162,31 +167,34 @@ public class ResolveClasspathsHandler {
      * @throws CoreException
      *             CoreException
      */
-    private static String[][] computeClassPath(IJavaProject javaProject) throws CoreException {
+    private static String[][] computeClassPath(IJavaProject javaProject, boolean includeTestScope)
+            throws CoreException {
         if (javaProject == null) {
             throw new IllegalArgumentException("javaProject is null");
         }
         String[][] result = new String[2][];
         if (JavaRuntime.isModularProject(javaProject)) {
-            result[0] = computeDefaultRuntimeClassPath(javaProject);
+            result[0] = computeDefaultRuntimeClassPath(javaProject, includeTestScope);
             result[1] = new String[0];
         } else {
             result[0] = new String[0];
-            result[1] = computeDefaultRuntimeClassPath(javaProject);
+            result[1] = computeDefaultRuntimeClassPath(javaProject, includeTestScope);
         }
         return result;
     }
 
-    private static String[] computeDefaultRuntimeClassPath(IJavaProject jproject) throws CoreException {
+    private static String[] computeDefaultRuntimeClassPath(IJavaProject jproject, boolean includeTestScope)
+            throws CoreException {
         IRuntimeClasspathEntry[] unresolved = JavaRuntime.computeUnresolvedRuntimeClasspath(jproject);
         Set<String> resolved = new LinkedHashSet<String>();
         for (int i = 0; i < unresolved.length; i++) {
             IRuntimeClasspathEntry entry = unresolved[i];
             if (entry.getClasspathProperty() == IRuntimeClasspathEntry.USER_CLASSES) {
-                IRuntimeClasspathEntry[] entries = JavaRuntime.resolveRuntimeClasspathEntry(entry, jproject, true);
+                IRuntimeClasspathEntry[] entries = JavaRuntime.resolveRuntimeClasspathEntry(entry, jproject,
+                        !includeTestScope);
                 for (int j = 0; j < entries.length; j++) {
 
-                    if (entries[j].getClasspathEntry().isTest() && !isRuntime(entries[j].getClasspathEntry())) {
+                    if (!includeTestScope && forTestOnly(entries[j].getClasspathEntry())) {
                         continue;
                     }
                     String location = entries[j].getLocation();
@@ -200,10 +208,73 @@ public class ResolveClasspathsHandler {
         return resolved.toArray(new String[resolved.size()]);
     }
 
-    private static boolean isRuntime(final IClasspathEntry classpathEntry) {
+    /**
+     * There is an issue on isTest: it will return true if the scope is runtime, so we will this method for testing whether
+     * the classpath entry is for test only.
+     *
+     * @param classpathEntry classpath entry
+     * @return whether this classpath entry is only used in test
+     */
+    private static boolean forTestOnly(final IClasspathEntry classpathEntry) {
         for (IClasspathAttribute attribute : classpathEntry.getExtraAttributes()) {
-            if (SCOPE_ATTRIBUTE.equals(attribute.getName()) && "runtime".equals(attribute.getValue())) {
-                return true;
+            if (GRADLE_SCOPE_ATTRIBUTE.equals(attribute.getName()) || MAVEN_SCOPE_ATTRIBUTE.equals(attribute.getName())) {
+                return TEST_SCOPE.equals(attribute.getValue());
+            }
+        }
+        return classpathEntry.isTest();
+    }
+
+    /**
+     * Test whether the main class is located in test folders.
+     * @param project the java project containing the main class
+     * @param mainClass the main class name
+     * @return whether the main class is located in test folders
+     */
+    private static boolean isMainClassInTestFolder(IJavaProject project, String mainClass) {
+        // get a list of test folders and check whether main class is here
+        int constraints = IJavaSearchScope.SOURCES;
+        List<IJavaElement> testFolders = new ArrayList<IJavaElement>();
+        try {
+            IPackageFragmentRoot[] packageFragmentRoot = project.getPackageFragmentRoots();
+            for (int i = 0; i < packageFragmentRoot.length; i++) {
+                if (packageFragmentRoot[i].getElementType() == IJavaElement.PACKAGE_FRAGMENT_ROOT
+                        && packageFragmentRoot[i].getKind() == IPackageFragmentRoot.K_SOURCE) {
+                    IClasspathEntry cpe = packageFragmentRoot[i].getResolvedClasspathEntry();
+                    if (forTestOnly(cpe)) {
+                        testFolders.add(packageFragmentRoot[i]);
+                    }
+                }
+            }
+        } catch (JavaModelException e) {
+            // ignore
+        }
+        if (!testFolders.isEmpty()) {
+            try {
+
+                List<Object> mainClassesInTestFolder = new ArrayList<>();
+                SearchPattern pattern = SearchPattern.createPattern(mainClass, IJavaSearchConstants.CLASS,
+                        IJavaSearchConstants.DECLARATIONS,
+                        SearchPattern.R_CASE_SENSITIVE | SearchPattern.R_EXACT_MATCH);
+                SearchEngine searchEngine = new SearchEngine();
+                IJavaSearchScope scope = SearchEngine
+                        .createJavaSearchScope(testFolders.toArray(new IJavaElement[0]), constraints);
+                SearchRequestor requestor = new SearchRequestor() {
+                    @Override
+                    public void acceptSearchMatch(SearchMatch match) {
+                        Object element = match.getElement();
+                        if (element instanceof IJavaElement) {
+                            mainClassesInTestFolder.add(element);
+                        }
+                    }
+                };
+
+                searchEngine.search(pattern, new SearchParticipant[] {
+                            SearchEngine.getDefaultSearchParticipant()
+                    }, scope, requestor, null /* progress monitor */);
+
+                return !mainClassesInTestFolder.isEmpty();
+            } catch (Exception e) {
+                logger.log(Level.SEVERE, String.format("Searching the main class failure: %s", e.toString()), e);
             }
         }
         return false;
