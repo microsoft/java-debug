0d2add2e3631770fdca78f3e99b8bf14476258c2
Jinbo Wang <jinbwan@microsoft.com>
2019-05-30 23:08:44 +0800

    Return the changed classes as response of redefine classes command
    Signed-off-by: Jinbo Wang <jinbwan@microsoft.com>

--- com.microsoft.java.debug.core/src/main/java/com/microsoft/java/debug/core/protocol/Responses.java
+++ com.microsoft.java.debug.core/src/main/java/com/microsoft/java/debug/core/protocol/Responses.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2017 Microsoft Corporation and others.
+* Copyright (c) 2017-2019 Microsoft Corporation and others.
 * All rights reserved. This program and the accompanying materials
 * are made available under the terms of the Eclipse Public License v1.0
 * which accompanies this distribution, and is available at
@@ -243,6 +243,17 @@ public class Responses {
         public ExceptionInfoResponse(String exceptionId, String description, ExceptionBreakMode breakMode, ExceptionDetails details) {
             this(exceptionId, description, breakMode);
             this.details = details;
+        }
+    }
+
+    public static class RedefineClassesResponse extends ResponseBody {
+        public String[] changedClasses = new String[0];
+
+        /**
+         * Constructor.
+         */
+        public RedefineClassesResponse(String[] changedClasses) {
+            this.changedClasses = changedClasses;
         }
     }
 }
