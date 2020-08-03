94284b0dbfb989a8145ffb05c32495037dbbf36c
Jinbo Wang <jinbwan@microsoft.com>
2020-08-03 13:28:13 +0800

    Dedup the changed classes before hot reload (#345)
    * Dedup the changed classes before hot reload
    
    Signed-off-by: Jinbo Wang <jinbwan@microsoft.com>
    
    * Make checkstyle happy

--- com.microsoft.java.debug.plugin/src/main/java/com/microsoft/java/debug/plugin/internal/JdtUtils.java
+++ com.microsoft.java.debug.plugin/src/main/java/com/microsoft/java/debug/plugin/internal/JdtUtils.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
@@ -398,5 +399,20 @@ public class JdtUtils {
             name.append("[]"); //$NON-NLS-1$
         }
         return name.toString();
+    }
+
+    /**
+     * Check whether two resources point to the same physical file.
+     */
+    public static boolean isSameFile(IResource resource1, IResource resource2) {
+        if (resource1 == null || resource2 == null) {
+            return false;
+        }
+
+        if (Objects.equals(resource1, resource2)) {
+            return true;
+        }
+
+        return Objects.equals(resource1.getLocation(), resource2.getLocation());
     }
 }
